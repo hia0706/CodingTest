ec119773ac86f8d3ae5fa6a242db01516b2717e9
Jinseo Lee <94761254+hia0706@users.noreply.github.com>
2024-06-09 08:49:01 +0900

    [Bronze I] Title: 2007년, Time: 176 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.java
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -3,18 +3,16 @@ import java.util.*;
 class Main {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
+        
         int month = in.nextInt();
         int day = in.nextInt();
-        in.close();
-        //  1 3 5 7 8 10 12
         int[] dayInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String[] dayOfTheWeeks = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+        String[] dayOfTheMonth = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
         
         int totalDays = day;
         for (int i = 0; i < month - 1; i++) {
             totalDays += dayInMonth[i];
         }
-        
-        System.out.println(dayOfTheWeeks[totalDays % 7]);
+        System.out.println(dayOfTheMonth[totalDays % 7]);
     }
 }
