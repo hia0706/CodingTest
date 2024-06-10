fbcacbc31feb1a4f01c50b2468932dcdd2b87783
Jinseo Lee <94761254+hia0706@users.noreply.github.com>
2024-06-10 15:00:57 +0900

    [Bronze V] Title: A+B - 2, Time: 116 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -1,12 +1,14 @@
-import java.util.*;
+import java.io.*;
 
 class Main {
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        
-        System.out.println(a + b);
-        
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+    public static int a,b;
+
+    public static void main(String[] args) throws IOException {
+        a = Integer.parseInt(br.readLine());
+        b = Integer.parseInt(br.readLine());
+        sb.append(a + b);
+        System.out.println(sb);
     }
 }
