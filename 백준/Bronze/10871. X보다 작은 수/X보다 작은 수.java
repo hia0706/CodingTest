bdec244dc207e28e278ac2bdf43661c460f2d1ee
Jinseo Lee <94761254+hia0706@users.noreply.github.com>
2024-06-13 11:21:55 +0900

    [Bronze V] Title: X보다 작은 수, Time: 196 ms, Memory: 16584 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,19 +1,26 @@
+import java.io.*;
 import java.util.*;
 
 class Main {
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        
-        int n = in.nextInt();
-        int x = in.nextInt();
-        int[] arr = new int[n];
-        
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+    public static int a,b;
+    public static String str;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
-            arr[i] = in.nextInt();
-            if (arr[i] < x) {
-                System.out.print(arr[i] + " ");
+            int r = Integer.parseInt(st.nextToken());
+            if (r < x) {
+                sb.append(r + " ");
             }
         }
-        in.close();
+        br.close();
+        System.out.println(sb);
     }
 }
