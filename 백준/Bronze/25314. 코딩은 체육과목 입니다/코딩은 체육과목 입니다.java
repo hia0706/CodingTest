e80e20c386c0301910dd51f8e0526451da7daced
Jinseo Lee <94761254+hia0706@users.noreply.github.com>
2024-06-13 11:49:32 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 128 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -1,15 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n/4; i++) {
-            System.out.print("long ");
+            sb.append("long ");
         }
-        System.out.println("int");
+        br.close();
+        System.out.println(sb + "int");
     }
 }
