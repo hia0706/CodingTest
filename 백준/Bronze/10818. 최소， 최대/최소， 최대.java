eb6ea36ed0b6d1144317b0fdfb49ff2cb3409952
Jinseo Lee <94761254+hia0706@users.noreply.github.com>
2024-06-18 17:54:17 +0900

    [Bronze III] Title: 최소, 최대, Time: 596 ms, Memory: 89300 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -5,18 +5,23 @@ class Main {
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static StringBuilder sb = new StringBuilder();
     public static StringTokenizer st;
-
-    public static void main(String[] args) throws IOException {
+    
+    public static void main(String args[]) throws IOException {
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
         st = new StringTokenizer(br.readLine(), " ");
-        int i = 0;
+        
+        int min = 1000000;
+        int max = -1000000;
         while (st.hasMoreTokens()) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            i++;
+            int num = Integer.parseInt(st.nextToken());
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
         }
         br.close();
-        Arrays.sort(arr);
-        System.out.println(arr[0] + " " + arr[n - 1]);
+        System.out.println(min + " " + max);
     }
 }
