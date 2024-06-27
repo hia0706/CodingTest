c6a796368f72e026f63797678cc21db0200801df
Jinseo Lee <94761254+hia0706@users.noreply.github.com>
2024-06-28 02:27:49 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String str = br.readLine();
+        
+        for (int i = 0; i < str.length(); i++) {
+            int ch = str.charAt(i);
+            int asc = ch;
+            System.out.print(asc);
+        }
+        br.close();
+    }
+}
